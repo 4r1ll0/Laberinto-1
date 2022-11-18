4a11bb430b44dc322e93c803780627ca67dfee86
Slazanor <73226150+Slazanor@users.noreply.github.com>
2022-11-18 12:59:32 +0000

    Mas cambios
    Trabajar en el script.

---
+++ Assets/MovimientoJugador.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        transform.Translate(-7.0f, 0.0f, 0.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
